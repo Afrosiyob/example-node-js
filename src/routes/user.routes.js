af5a23fd6d24b4c9e059584f9f328f714202a8d9
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-07-14 13:03:52 +0500

    add
    add

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,17 +1,22 @@
 const { Router } = require("express");
-const { createUser, getUsers, getUser, updateUser, deleteUser } = require("../controllers/user.controller");
+const {
+    createUser,
+    getUsers,
+    getUser,
+    updateUser,
+    deleteUser,
+} = require("../controllers/user.controller");
 const { validationError } = require("../middlewares/middlewares");
 const { userCreateValidation } = require("../validations/validations");
 
 const router = Router();
 
-
-router.post("/create", userCreateValidation, validationError, createUser)
-router.get("/", getUsers)
-router.get("/:userId", getUser)
-router.put("/:userId", updateUser)
-router.delete("/userId", deleteUser)
+router.post("/create", userCreateValidation, validationError, createUser);
+router.get("/", getUsers);
+router.get("/:userId", getUser);
+router.put("/:userId", updateUser);
+router.delete("/userId", deleteUser);
 
 module.exports = {
-    userRouter: router
-}
+    userRouter: router,
+};
