6a915251215caa665440f7fc52e34cda25e44246
Afrosiyob <afrosiyob.a97@gmail.com>
2021-07-14 23:40:52 +0500

    add
    add

--- src/errors/errors.js
+++ src/errors/errors.js
@@ -1,34 +1,35 @@
-const { logger } = require("../logs/logs");
+const { logger } = require( "../logs/logs" );
 
-const handleErrors = async(err, req, res, next) => {
+const handleErrors = async ( err, req, res, next ) => {
     const { message } = err;
-    switch (message) {
+    switch ( message ) {
         case "NO_ROLE":
-            return await res.status(403).json({ error: message });
+            return await res.status( 403 ).json( { error: message } );
         case "NO_PERMESSION":
-            return await res.status(403).json({ error: message });
+            return await res.status( 403 ).json( { error: message } );
         case "PASSWORD_INCORRECT":
             return await res
-                .status(400)
-                .json({ error: message, message: "password incorrect" });
+                .status( 400 )
+                .json( { error: message, message: "password incorrect" } );
         case "NO_USER":
-            res.status(400).json({ error: message, message: "no user" });
+            res.status( 400 ).json( { error: message, message: "no user" } );
         case "SAME_NAME":
             return await res
-                .status(400)
-                .json({ error: message, message: `please enter other name` });
+                .status( 400 )
+                .json( { error: message, message: `please enter other name` } );
         case "NO_FILE":
-            return await res.status(400).json({
+            return await res.status( 400 ).json( {
                 error: message,
                 message: "please enter file",
-            });
+            } );
         case "SAME_USERNAME":
-            logger.error("same username");
+            logger.error( "same username" );
             return await res
-                .status(400)
-                .json({ error: message, message: "enter other username" });
+                .status( 400 )
+                .json( { error: message, message: "enter other username" } );
         default:
-            return await res.status(500).json({ message: "server error new" });
+            console.log( err );
+            return await res.status( 500 ).json( { message: "server error new" } );
     }
 };
 
